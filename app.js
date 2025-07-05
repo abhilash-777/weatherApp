3d447e9fe72317fcd74b1bf6c9c7f8ba1a079fe5
abhilash-777 <abhilashguptha626@gmail.com>
2025-07-05 15:51:01 +0530

    Flatten folder structure for Render deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const path = require('path');
 
 const app = express();
-const weatherData = require('../utils/weatherData');
+const weatherData = require('./utils/weatherData');
 
 const publicPath = path.join(__dirname,"../public");
 const viewPath = path.join(__dirname,"../templates/views");
